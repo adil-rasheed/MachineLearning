7cb14ba151806ebeda263ab197b7d53d89e6b086
adil-rasheed <rushtoadil@gmail.com>
2017-07-03 03:30:55 +0200

    Everything Working

--- Regression/tf_regression.py
+++ Regression/tf_regression.py
@@ -1,50 +1,19 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jul  2 10:34:21 2017
-
-@author: arash
-"""
 import numpy as np
-import tensorflow as tf
-def featureNormalize(X):
-    X_norm = X
-    mu    = np.zeros((1, X.shape[1]))
-    sigma = np.zeros((1, X.shape[1]))
-    for i in range(X.shape[1]):
-        mu[:,i] = np.mean(X[:,i])
-        sigma[:,i] = np.std(X[:,i])
-        X_norm[:,i] = (X[:,i] - float(mu[:,i]))/float(sigma[:,i])
-    return X_norm, mu, sigma
+import scipy.io as sio
+import matplotlib.pyplot as plt
+from sklearn import datasets,linear_model
+diabetes = datasets.load_diabetes()
+# Use only one feature
+diabetes_X = diabetes.data[:, np.newaxis, 2]
 
-data = np.loadtxt('data.txt', delimiter=",")
-X = data[:,:2]
-y = data[:,2]
-y=y.reshape(-1,1)
-X_norm, mu, sigma = featureNormalize(X)
-mx,nx = X.shape
-my,ny = y.shape
-x_ph = tf.placeholder(tf.float32, shape = [None, nx])
-y_ph = tf.placeholder(tf.float32, shape = [None, ny])
+X = diabetes_X[:-20]
+X=np.column_stack((np.ones((X.shape[0],1)), X))
+y = diabetes.target[:-20]#.reshape(-1,1)
+plt.scatter(X[:,1], y,  color='black')
+m,n=X.shape
+theta = np.zeros((X.shape[1],1))
+num_iters=1000
+alpha=0.1
 
-W = tf.Variable(tf.random_normal([nx,1]), name="Weight")
-b = tf.Variable(tf.random_normal([ny,1]), name="Intercept")
-
-hypothesis = tf.matmul(x_ph, W) + b
-init = tf.global_variables_initializer()
-Loss = tf.reduce_mean(tf.square(y_ph - hypothesis))
-optimizer = tf.train.GradientDescentOptimizer(learning_rate= 0.01)
-train = optimizer.minimize(Loss)
-sess = tf.Session()
-# Initialize all the variables
-sess.run(init)
-#sess.run(train,feed_dict = {x_ph:X, y_ph:y})
-cost_val, hy_val, W_val, b_val, _ = sess.run([Loss, hypothesis, W, b, train],feed_dict = {x_ph:X, y_ph:y})
-house_norm_padded = np.array([1650, 3])
-#for epochs in range(40000):
-#    cost_val, hy_val, W_val, b_val, _ = sess.run([Loss, hypothesis, W, b, train],feed_dict = {x_ph:X_norm, y_ph:y})
-#    
-#    if epochs % 100 == 0:
-#        print(epochs, cost_val, W_val, b_val)
-        
-price2 = np.array(house_norm_padded).dot(W_val)+b_val
-print("Predicted price of a 1650 sq-ft, 3 br house (using normal equation):", price2)
+for i in range(num_iters):
+    theta = theta -alpha/2/m*np.transpose(X).dot(X.dot(theta) - np.transpose([y]))
